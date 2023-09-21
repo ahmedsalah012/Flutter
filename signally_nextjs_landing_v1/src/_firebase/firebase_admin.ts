13d5e3d6cda6f2c96bebe9100de591b3839b079d
Ahmed <89715356+ahmedsalah012@users.noreply.github.com>
2023-09-21 23:05:04 +0300

    Update firebase_admin.ts

--- signally_nextjs_landing_v1/src/_firebase/firebase_admin.ts
+++ signally_nextjs_landing_v1/src/_firebase/firebase_admin.ts
@@ -3,7 +3,9 @@ import * as admin from 'firebase-admin';
 export const firebaseConfigServer = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n')
+    privateKey: process.env.FIREBASE_PRIVATE_KEY
+          ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
+          : undefined
 };
 
 const app = getApp();
